c1da460fdcde942423fe060ae83fdcf38188ba4b
Milan Hauth <milahu@gmail.com>
2022-01-10 16:29:48 +0100

    fix find-xargsparallel-sed.sh

--- tests/find-xargsparallel-sed.sh
+++ tests/find-xargsparallel-sed.sh
@@ -8,8 +8,7 @@ bench_start
 
 num_parallel=2
 
-find testfiles/ -type f -print0 | xargs -0 -P $num_parallel sed -i 's/a/a/g'
-find testfiles/ -type f -print0 | xargs -0 -P $num_parallel sed -i 's/a/a/g'
+find testfiles/ -type f -print0 | xargs -0 -P $num_parallel sed -i 's/a/A/g'
+find testfiles/ -type f -print0 | xargs -0 -P $num_parallel sed -i 's/A/a/g'
 
 bench_stop $(basename "$0")
-
